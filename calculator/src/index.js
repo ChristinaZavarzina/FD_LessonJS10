e2878aa1f26db99d88865309746014227af32a0a
Kristina <kristinazavarzina2020@gmail.com>
2023-05-13 19:52:09 +0300

    less10hw

---
+++ calculator/src/index.js
@@ -0,0 +1,83 @@
+/*
+* У папці calculator дана верстка макета калькулятора. 
+Потрібно зробити цей калькулятор робочим.
+* При натисканні на клавіші з цифрами - набір введених цифр має бути показаний на табло калькулятора.
+* При натисканні на знаки операторів (`*`, `/`, `+`, `-`) на табло нічого не відбувається - програма чекає введення другого числа для виконання операції.
+* Якщо користувач ввів одне число, вибрав оператор і ввів друге число, то при натисканні як кнопки `=`, так і будь-якого з операторів, в табло повинен з'явитися результат виконання попереднього виразу.
+* При натисканні клавіш `M+` або `M-` у лівій частині табло необхідно показати маленьку букву `m` - це означає, що в пам'яті зберігається число. Натискання на MRC покаже число з пам'яті на екрані. Повторне натискання `MRC` має очищати пам'ять.
+*/
+
+'use strict'
+
+const display = document.querySelector('.display');
+const buttons = document.querySelectorAll('.button');
+const mrcButton = document.querySelector('.button__mrc');
+const mMinusButton = document.querySelector('.button__m-');
+const mPlusButton = document.querySelector('.button__m');
+const eqButton = document.getElementById('eq');
+
+let firstNum = '';
+let secondNum = '';
+let operator = '';
+let memory = null;
+
+buttons.forEach(button => {
+  button.addEventListener('click', () => {
+    const value = button.value;
+    if (value === '+' || value === '-' || value === '*' || value === '/') {
+      operator = value;
+    } else if (operator === '') {
+      firstNum += value;
+      display.value = firstNum;
+    } else {
+      secondNum += value;
+      display.value = secondNum;
+    }
+  });
+});
+
+eqButton.addEventListener('click', () => {
+  let result;
+  switch (operator) {
+    case '+':
+      result = parseFloat(firstNum) + parseFloat(secondNum);
+      break;
+    case '-':
+      result = parseFloat(firstNum) - parseFloat(secondNum);
+      break;
+    case '*':
+      result = parseFloat(firstNum) * parseFloat(secondNum);
+      break;
+    case '/':
+      result = parseFloat(firstNum) / parseFloat(secondNum);
+      break;
+  }
+  display.value = result;
+  firstNum = result.toString();
+  secondNum = '';
+  operator = '';
+});
+
+document.querySelector('.button.white').addEventListener('click', () => {
+  display.value = '';
+  firstNum = '';
+  secondNum = '';
+  operator = '';
+});
+
+mrcButton.addEventListener('click', () => {
+  if (memory !== null) {
+    display.value = memory.toString();
+    memory = '';
+  }
+});
+
+mMinusButton.addEventListener('click', () => {
+  memory = null;
+  display.textContent = '';
+});
+
+mPlusButton.addEventListener('click', () => {
+  memory = parseFloat(display.value);
+  display.textContent = 'm';
+});
